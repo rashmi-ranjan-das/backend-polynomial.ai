503ea300d4f620a25d838461f9e1b7cddf0c17b3
Rashmi Ranjan Das <62706300+WebDesign4New@users.noreply.github.com>
2022-02-17 18:33:58 +0530

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -31,7 +31,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router_text.urls)),
     path('api/', include(router_ip.urls)),
-
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
